75e4106f0675229d5c71547e333d6ea4fa0e4150
shyam sardhara <80083773+shyam1112@users.noreply.github.com>
2023-04-21 10:18:55 +0530

    Create 41.First Missing Positive.java

---
+++ 41.First Missing Positive.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int firstMissingPositive(int[] arr) {
+        
+        int i=0;
+        while(i<arr.length){
+            int correct=arr[i]-1;
+            if(arr[i]>0 && arr[i]<arr.length && arr[i]!=arr[correct]){
+                swap(arr,i,correct);
+            }else{
+                i++;
+            }
+        }
+
+        for(int j=0;j<arr.length;j++){
+            if(arr[j]!=j+1){
+                return j+1;
+            }
+        }
+        return arr.length+1;
+    }
+    static void swap(int[] arr,int i,int correct){
+        int temp=arr[i];
+        arr[i]=arr[correct];
+        arr[correct]=temp;
+    }
+    
+}
