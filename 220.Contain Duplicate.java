5c019fd5fed393c5a8eea9172bb1be79b9abd877
shyam sardhara <80083773+shyam1112@users.noreply.github.com>
2023-04-21 23:13:33 +0530

    Update 220.Contain Duplicate.java

--- 220.Contain Duplicate.java
+++ 220.Contain Duplicate.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         
         Map<Integer,Integer> hm=new HashMap<>();
-
         for(int i=0;i<nums.length;i++){
         if(!hm.containsKey(nums[i])){
             hm.put(nums[i],i);
