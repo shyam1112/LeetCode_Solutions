8470e2f71c4097fff7b4c53e9a3e4174fa77ad5e
shyam sardhara <80083773+shyam1112@users.noreply.github.com>
2023-05-11 22:00:54 +0530

    Update 11. Container With Most Water.java

--- 11. Container With Most Water.java
+++ 11. Container With Most Water.java
@@ -1,9 +1,11 @@
 class Solution {
     public int maxArea(int[] height) {
+        //starting point
         int l=0;
         int r=height.length-1;
         int max=0;
         while(l<r){
+            //the lh=the height of the l array and rh=the height of the r
             int lh=height[l];
             int rh=height[r];
             int min_h=Math.min(lh,rh);
