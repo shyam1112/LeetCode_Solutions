f79898c9e190e378f9447fcaccc9b325848976b9
shyam sardhara <80083773+shyam1112@users.noreply.github.com>
2023-05-12 22:39:00 +0530

    Update 29.Divide Two integers.java

--- 29.Divide Two integers.java
+++ 29.Divide Two integers.java
@@ -1,8 +1,6 @@
 class Solution {
     public int divide(int dividend, int divisor) {
-        // if(dividend<0 && divisor>0){
-        //     dividend=dividend*-1;
-        // }
+     
          if (divisor == 0 || (dividend == Integer.MIN_VALUE && divisor == -1)) {
             return Integer.MAX_VALUE;
         }
