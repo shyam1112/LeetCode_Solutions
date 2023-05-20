cca298a4314b620b3c8462139905823527d8e190
shyam sardhara <80083773+shyam1112@users.noreply.github.com>
2023-05-20 22:35:18 +0530

    Update 279.Perfect_Squares.java

--- 279.Perfect_Squares.java
+++ 279.Perfect_Squares.java
@@ -5,6 +5,7 @@ class Solution {
         dp[1]=1;
 
         for(int i=2;i<=n;i++){
+            
             int min=Integer.MAX_VALUE;
             for(int j=1;j*j<=i;j++){
                 int rem=i-j*j;
