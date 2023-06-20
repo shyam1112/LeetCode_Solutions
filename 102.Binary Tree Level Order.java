d6c23a7b539dc389d08f557c6e70a4423621e7c3
shyam sardhara <80083773+shyam1112@users.noreply.github.com>
2023-06-20 22:01:15 +0530

    Create 102.Binary Tree Level Order.java

---
+++ 102.Binary Tree Level Order.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue= new LinkedList<TreeNode>();
+        List<List<Integer>> list= new LinkedList<List<Integer>>();
+
+        if(root==null){
+            return list;
+        }
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            List<Integer> sublist= new LinkedList<Integer>();
+            for(int i=0;i<size;i++){
+                if(queue.peek().left != null){
+                    queue.add(queue.peek().left);
+                }
+                if(queue.peek().right != null){
+                    queue.add(queue.peek().right);
+                }
+                sublist.add(queue.poll().val);
+            }
+            list.add(sublist);
+        }
+        return list;
+    }
+}
