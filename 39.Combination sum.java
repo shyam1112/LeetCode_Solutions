f0101bc39e5fba20660bd864f31d78a002247e40
shyam sardhara <80083773+shyam1112@users.noreply.github.com>
2023-07-07 22:39:01 +0530

    Create 39.Combination sum.java

---
+++ 39.Combination sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans= new ArrayList<>();
+        findCombination(0,candidates,target,ans,new ArrayList<>());
+        return ans;
+    }
+    public static void findCombination(int idx,int[] arr,int target,List<List<Integer>> ans,List<Integer> ds){
+        if(idx==arr.length){
+            if(target==0){
+                ans.add(new ArrayList<>(ds));
+            }
+            return;
+        }
+
+        if(arr[idx]<=target){
+            ds.add(arr[idx]);
+            findCombination(idx,arr,target-arr[idx],ans,ds);
+            ds.remove(ds.size()-1);
+        }
+        findCombination(idx+1,arr,target,ans,ds);
+
+    }
+}
